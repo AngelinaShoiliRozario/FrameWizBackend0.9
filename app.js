ae21f2f7402a7fb0b13fdede1cb8bb06869b7e4d
AngelinaShoiliRozario <shoili.rozario@gmail.com>
2023-12-03 16:29:23 +0600

    dd

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 0;
 const cors = require('cors');
 const session = require('express-session'); 
 const flash = require('connect-flash'); 
